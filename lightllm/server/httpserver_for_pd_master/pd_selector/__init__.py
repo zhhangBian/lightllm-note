6922a6616600b300006873373a87126c2e1b061b
pigKiller <22373017@buaa.edu.cn>
2025-07-16 11:30:30 +0800

    fix: create func para

--- lightllm/server/httpserver_for_pd_master/pd_selector/__init__.py
+++ lightllm/server/httpserver_for_pd_master/pd_selector/__init__.py
@@ -1,5 +1,5 @@
 from typing import List
-from lightllm.server.httpserver_for_pd_master.manager import PD_Client_Obj
+from lightllm.server.httpserver_for_pd_master.manager import PD_Client_Obj, PDManager
 from .pd_selector import (
     PDSelector,
     RandomSelector,
@@ -16,14 +16,14 @@ __all__ = [
     "RadixSelector"
 ]
 
-def create_selector(selector_type: str, prefill_nodes: List[PD_Client_Obj], decode_nodes: List[PD_Client_Obj]) -> PDSelector:
+def create_selector(selector_type: str, prefill_nodes: List[PD_Client_Obj], decode_nodes: List[PD_Client_Obj], pd_manager: PDManager) -> PDSelector:
     if selector_type == "random":
-        return RandomSelector(prefill_nodes, decode_nodes)
+        return RandomSelector(prefill_nodes, decode_nodes, pd_manager)
     elif selector_type == "round_robin":
-        return RoundRobinSelector(prefill_nodes, decode_nodes)
+        return RoundRobinSelector(prefill_nodes, decode_nodes, pd_manager)
     elif selector_type == "memory":
-        return MemorySelector(prefill_nodes, decode_nodes)
+        return MemorySelector(prefill_nodes, decode_nodes, pd_manager)
     elif selector_type == "radix":
-        return RadixSelector(prefill_nodes, decode_nodes)
+        return RadixSelector(prefill_nodes, decode_nodes, pd_manager)
     else:
         raise ValueError(f"Invalid selector type: {selector_type}")
