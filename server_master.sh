e983ce8303004a0c1502d0b32d09c36f962b76ec
pigKiller <22373017@buaa.edu.cn>
2025-07-28 17:20:43 +0800

    remove unnecessary files

--- server_master.sh
+++
@@ -1,6 +0,0 @@
-python3 -m lightllm.server.api_server \
-    --model_dir /mtc/bianzhuohang/models/Qwen/Qwen2.5-14B \
-    --run_mode "pd_master" \
-    --host 127.0.0.1 \
-    --port 60011 \
-    --select_p_d_node_func random
