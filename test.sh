7f1dd4fbeebb12b885e570c7030d5ff3bdb8c54b
pigKiller <22373017@buaa.edu.cn>
2025-08-11 15:50:05 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+python3 test/benchmark/service/benchmark_client.py \
+  --url http://127.0.0.1:60011/generate_stream \
+  --tokenizer_path /mtc/bianzhuohang/models/Qwen/Qwen2.5-14B \
+  --server_api lightllm \
+  --dump_file result.json \
+  --seed 42 \
+  --input_len 4096 \
+  --output_len 1024 \
+  --input_num 1000
